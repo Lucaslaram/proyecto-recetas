f4a6caa9f42f905c5a338648585b218e47afc2b8
Lucaslaram <lularamorales16@gmail.com>
2026-06-03 13:43:14 -0500

    Modelos de Base de Datos Modulares

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from app.models import Base, engine
+
+# Crear de forma automática todas las tablas registradas en el __init__
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Generador de Recetas",
     description="API para generar recetas con IA a partir de tu inventario",
     version="1.0.0"
 )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/health")
 def health_check():
